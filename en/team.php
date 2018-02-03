33fa5a4c5c4f111c18d7db1851fae9cb81f60f6d
Ge Jian Feng <gejianfeng2@126.com>
2018-02-04 00:14:50 +0800

    + team done

---
+++ en/team.php
@@ -0,0 +1,179 @@
+<?php
+/*
+Template Name: team.php
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="team">
+	<div class="header">
+		<?php get_top_banner(); ?>
+	</div>
+
+	<div id="member">
+		<div class="wrap">
+			<div class="title">Team</div>
+
+			<div class="title-content">
+				We have a team of members with rich knowledge in technology, theory and career experiences in live event markets. Some team members have rich knowledge, insights and experiences in live event and live stream markets. The strong background of the team will ensure a business model with high feasibility, promising prospect, and robust technical design.
+			</div>
+			
+			<div class="frame frame1">
+				<div class="icon icon1"></div>
+				<div class="frame-content">
+					<div class="name">
+						Yifeng Mao, CEO
+					</div>
+					<div class="intro">
+						Mr. Mao is an expert in blockchain application and data application. He established a world-leading data application firm, Goldpebble Research, in 2010, and set up the largest live event project private equity fund in China. He has a good knowledge about how to combine blockchain with live events.  Since 2012, he has become a partner of Fortress, which is one of the first institutional investors in Bitcoin. Mr. Mao is also the initiator of Intelligent Blockchain Research Center in Shanghai Jiao Tong University.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame2">
+				<div class="icon icon2"></div>
+				<div class="frame-content">
+					<div class="name">
+						James Qu, CTO
+					</div>
+					<div class="intro">
+						Mr. Qu is an expert in blockchain and data processing, and also one of the initiators of JUICE Blockchain Platform. Mr. Qu was technical director of JUZIX, partner of Genus Tech, and had been working for Morgan Stanley for 15 years before that.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame3">
+				<div class="icon icon3"></div>
+				<div class="frame-content">
+					<div class="name">
+						Isaac Kang, Data Analyzing in Live Event
+					</div>
+					<div class="intro">
+						Mr. Kang is not only an expert in data analysis, but also in Chinese live streaming industry. He established the first big data model to analyze, monitor and estimate live event market. Together with Mr. Mao, he established the largest live event project private equity fund in China, and is the fund manager. He has helped tens of artists to reach the top 200 in multiple live streaming platforms.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame4">
+				<div class="icon icon4"></div>
+				<div class="frame-content">
+					<div class="name">
+						Felicia Bao, Ecosystem
+					</div>
+					<div class="intro">
+						Ms. Bao is among the very first to analyze Bitcoin ecosystem with classical economic view. She has issued a fundamental research report regarding Bitcoin earlier than major wall-street banks.  She graduated from Economic Department of Fudan University.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame5">
+				<div class="icon icon5"></div>
+				<div class="frame-content">
+					<div class="name">
+						Zhengwei Ye, Artist Manager Network
+					</div>
+					<div class="intro">
+						Mr. Ye is responsible for artist manager network for LiveCoins. He worked for Hangzi Artist Management Company, where he helped developing relationship with artists and artist managers.  He has cooperated with more than 1000 artists, and understands the true needs of successful live events from artists’ perspective.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame6">
+				<div class="icon icon6"></div>
+				<div class="frame-content">
+					<div class="name">
+						Lijuan Han, Live Streaming Artist Network
+					</div>
+					<div class="intro">
+						Ms. Han is responsible for live streaming artist network for LiveCoins. She is one of the first managers in Chinese live streaming industry. She has been working with dozens of live streaming artists in China, and helped a few live streaming artists improve their ranking from 1000th to top 20.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div id="pantner">
+		<div class="wrap">
+			<div class="title">Pantner</div>
+			<div class="title-content">
+				We have a team of members with rich knowledge in technology, theory and career experiences in live event markets. Some team members have rich knowledge, insights and experiences in live event and live stream markets. The strong background of the team will ensure a business model with high feasibility, promising prospect, and robust technical design.
+			</div>
+
+			<div class="frame frame1">
+				<div class="icon icon1"></div>
+				<div class="frame-content">
+					<div class="name">
+						Zhai Guangtao, PHD, Blockchain and Smart Contract
+					</div>
+					<div class="intro">
+						Dr. Zhai is a research professor of Shanghai Jiao Tong University. He is an expert of artificial intelligence and blockchain. His papers have been quoted for more than 2000 times in past 10 years according to Google Scholar. He is a co-founder of Intelligent Blockchain Research Center in Shanghai Jiao Tong University.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame2">
+				<div class="icon icon2"></div>
+				<div class="frame-content">
+					<div class="name">
+						Jeff Cui, Founder and CEO of Tking.com
+					</div>
+					<div class="intro">
+						Mr. Cui worked for Morgan Stanley and was the leading architecture of MS Global Trading System.  He founded Tking, a ticket exchange in 2015. In November 2017, Tking raised its round B financing of $40mn, a record breaking number in this sector. Since establishment, Tking has sold more than 2.5mn tickets, among which 92% are discounted. The platform has helped fans save more than RMB 100mn, improve attendance rate by 15%-20% generally, and enable more people to enjoy performances.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame3">
+				<div class="icon icon3"></div>
+				<div class="frame-content">
+					<div class="name">
+						Sun Liyong, Founder and CEO of Piaoniu.com 
+					</div>
+					<div class="intro">
+						Mr. Sun worked for Tencent and Dianping as product manager before founding Piaoniu, one of the earliest ticket exchanges in China. Until now, Piaoniu has got invested by SIG, Tou Tou Shi Dao (investment fund), Starling VC, Light-Up Fund, 500.com, and so on. It has just finished its round B Financing of $10mn+. Piaoniu has covered more than 50 cities and more than 2000 live events in past year.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame4">
+				<div class="icon icon4"></div>
+				<div class="frame-content">
+					<div class="name">
+						Patrick Gu, Initiator of Huai Shi Culture
+					</div>
+					<div class="intro">
+						Mr. Gu, is the initiator of Huai Shi Culture. He worked for world largest ticketing platform Ticketmaster and was the general manager of Ticketmaster China. Before that, he worked as a senior database developer for Ticketmaster US.
+					</div>
+				</div>
+			</div>
+
+			<div class="frame frame5">
+				<div class="icon icon5"></div>
+				<div class="frame-content">
+					<div class="name">
+						Yi Xu, PHD, Asssitant Researcher, ICCI, SJTU
+					</div>
+					<div class="intro">
+						Dr. Xu is assistant researcher of institute of culture and creative industry. She specializes in studies of live streaming artist and fans relations and the culture development of MAG (Manga, Anime and Games) sector.
+					</div>
+				</div>
+			</div>
+
+			<div class="orgicon oi1"></div>
+			<div class="orgicon oi2"></div>
+			<div class="orgicon oi3"></div>
+			<div class="orgicon oi4"></div>
+			<div class="orgicon oi5"></div>
+			<div class="orgicon oi6"></div>
+			<div class="orgicon oi7"></div>
+			<div class="orgicon oi8"></div>
+			<div class="orgicon oi9"></div>
+			<div class="orgicon oi10"></div>
+			<div class="orgicon oi11"></div>
+			<div class="orgicon oi12"></div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
