5dd47a525c60dacdd07dda024e91308e94857595
Ge Jian Feng <gejianfeng2@126.com>
2018-02-04 01:46:19 +0800

    + whattosolve done

---
+++ en/whattosolve.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Template Name: whattosolve.php
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="whattosolve">
+	<div class="header">
+		<?php get_top_banner(); ?>
+
+		<div class="wrap">
+			<div class="title">Problems with <br/>Current Live Event Markets</div>
+
+			<div class="titleicon ti1">
+				<div class="titext">Inefficiency</div>
+			</div>
+			<div class="titleicon ti2">
+				<div class="titext">Dominant Power</div>
+			</div>
+			<div class="titleicon ti3">
+				<div class="titext">Incentive Incompatible</div>
+			</div>
+			<div class="titleicon ti4">
+				<div class="titext">Opaque</div>
+			</div>
+			<div class="titleicon ti5">
+				<div class="titext">Rent-Seeking</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="content">
+		<div class="wrap">
+			<div class="line1"></div>
+			<div class="line2"></div>
+			<div class="line3"></div>
+			<div class="line4"></div>
+
+			<div class="container c1">
+				<div class="container-title ct1">
+					Artist
+				</div>
+
+				<div class="detail-container">
+					<div class="detail">Eroded Returns and Incentives by Intermediaries</div>
+					<div class="detail">Few Access to Funding the live Events</div>
+					<div class="detail">Lose Incentives to Improve Art Work after Selling It</div>
+				</div>
+			</div>
+
+			<div class="container c2">
+				<div class="container-title ct2">
+					Service Providers
+				</div>
+
+				<div class="detail-container">
+					<div class="detail">Service Providers with Functions of Promoters have Dominant Power</div>
+					<div class="detail">Lack of New Events to Provide Services for</div>
+				</div>
+			</div>
+
+			<div class="container c3">
+				<div class="container-title ct3">
+					Fans
+				</div>
+
+				<div class="detail-container">
+					<div class="detail">Pay More to Intermediaries Rather than Artists</div>
+					<div class="detail">No Safe Channel to Buy Tickets Directly form Artists (Producers)</div>
+					<div class="detail">No Minority Artist Event Available for Their Fans</div>
+				</div>
+			</div>
+
+			<div class="container c4">
+				<div class="container-title ct4">
+					Service Providers<br/><font size="5">( Promoters, Scalpers, Ticketing Platforms )</font>
+				</div>
+
+				<div class="detail-container">
+					<div class="detail">High Concentration of Promoters Leads to Dominant Bargaining Power</div>
+					<div class="detail">Ticketing Platforms Monopoly Brings Inefficiency</div>
+					<div class="detail">Manipulation of Ticket Supply and Market Prices</div>
+					<div class="detail">Profit Not Proportional to the Contribution</div>
+					<div class="detail">Inefficient Promoting Campaigns</div>
+					<div class="detail">Failure in Optimal Location Decision and Mispricing</div>
+					<div class="detail">Scalpers Default on Ticket Delivery</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
