9379c76557b71cbcce37bbf3b98f430d1698eb1a
Ge Jian Feng <gejianfeng2@126.com>
2018-02-07 01:57:49 +0800

    + faq not finished

---
+++ en/faq.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: faq.php
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="faq">
+	<div class="header">
+		<?php get_top_banner(); ?>
+
+		<div class="wrap">
+			<div class="title">Frequently Asked Questions</div>
+		</div>
+	</div>
+
+	<div class="content">
+		<div class="wrap">
+			
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
