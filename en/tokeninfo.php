91481865a95f18886220fe5cc2d43c70e928e698
Ge Jian Feng <gejianfeng2@126.com>
2018-02-07 01:30:09 +0800

    + tokeninfo done

---
+++ en/tokeninfo.php
@@ -0,0 +1,114 @@
+<?php
+/*
+Template Name: tokeninfo.php
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="tokeninfo">
+	<div class="header">
+		<?php get_top_banner(); ?>
+
+		<div class="wrap">
+			<div class="title">What I can do with LiveCoin?</div>
+
+			<div class="tf1 title-frame">
+				<div class="title-icon ti1"></div>
+				<div class="title-frame-container">
+					<div class="title-frame-content">
+						Purchase EventPacks to exchange for tickets and revenue share of a live event later
+					</div>
+				</div>
+			</div>
+
+			<div class="tf2 title-frame">
+				<div class="title-icon ti2"></div>
+				<div class="title-frame-container">
+					<div class="title-frame-content">
+						Purchase Services from Service Providers
+					</div>
+				</div>
+			</div>
+
+			<div class="tf3 title-frame">
+				<div class="title-icon ti3"></div>
+				<div class="title-frame-container">
+					<div class="title-frame-content">
+						Enjoy Additional Priorities and Benefits in a Live Event
+					</div>
+				</div>
+			</div>
+
+			<div class="tf4 title-frame">
+				<div class="title-icon ti4"></div>
+				<div class="title-frame-container">
+					<div class="title-frame-content">
+						More is Coming
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="content">
+		<div class="wrap">
+			<div class="content-title content-title-1">TOKEN INFORMATION</div>
+			<div class="content-table-title-l">1st Round Private Sale Tile:<br/>Late in January 2018</div>
+			<div class="content-table-title-r">2st Round Private Sale Tile:<br/>Second Quarter of 2018</div>
+			<div class="content-gray-line content-table-title-line"></div>
+
+			<div class="content-gray-line content-table-l1"></div>
+			<div class="content-gray-line content-table-l2"></div>
+			<div class="content-gray-line content-table-l3"></div>
+			<div class="content-gray-line content-table-l4"></div>
+			<div class="content-gray-line content-table-l5"></div>
+			<div class="content-gray-line content-table-l6"></div>
+			<div class="content-gray-line content-table-l7"></div>
+			<div class="content-gray-line content-table-l8"></div>
+			<div class="content-gray-line content-table-l9"></div>
+
+			<div class="content-table-row-left content-table-r1">Total Token Supply</div><div class="content-table-row-right content-table-r1">2,000,000,000 LIN</div>
+			<div class="content-table-row-left content-table-r2">Token Available</div><div class="content-table-row-right content-table-r2">1,200,000,000 LIN</div>
+			<div class="content-table-row-left content-table-r3">Initial Price</div><div class="content-table-row-right content-table-r3">1 LIN = $0.01</div>
+			<div class="content-table-row-left content-table-r4">1st Round of Private Sale Ratio</div><div class="content-table-row-right content-table-r4">40% of total supply of tokens</div>
+			<div class="content-table-row-left content-table-r5">2nd Round of Private Sale Ratio</div><div class="content-table-row-right content-table-r5">20% of total supply of tokens</div>
+			<div class="content-table-row-left content-table-r6">Hard Cap</div><div class="content-table-row-right content-table-r6">$ 12,000,000</div>
+			<div class="content-table-row-left content-table-r7">Soft Cap</div><div class="content-table-row-right content-table-r7">$ 6,000,000</div>
+			<div class="content-table-row-left content-table-r8">Token Format</div><div class="content-table-row-right content-table-r8">ERC-20</div>
+			<div class="content-table-row-left content-table-r9">Accepted Payments</div>
+
+			<div class="btc-icon"></div>
+			<div class="btc-name content-table-r9">BTC , BNB</div>
+			<div class="eth-icon"></div>
+			<div class="eth-name content-table-r9">ETH</div>
+
+			<div class="content-title content-title-2">TOKEN &amp; FUNDS ALLOCATION</div>
+
+			<div class="chart chart1">
+				<div class="chart-inner">
+					<div class="chart-inner-cell">Token <br/>Allocation</div>
+				</div>
+			</div>
+			<div class="chart chart2">
+				<div class="chart-inner">
+					<div class="chart-inner-cell">Funds <br/>Allocation</div>
+				</div>
+			</div>
+
+			<div class="chart-frame cf1">
+				<div class="cf-title">LIN Token Allocation</div>
+				<div class="cf-content">1% of the funds collected by artists from issuing EventPacks will be extracted as a commission of LiveCoins. These LINs tokens collected will be burnt right away to support the value of LINs in the market.</div>
+				<div class="cf-content-image cfci1"></div>
+			</div>
+
+			<div class="chart-frame cf2">
+				<div class="cf-title">LIN Funds Allocation</div>
+				<div class="cf-content">An import ant usage of the funds is artists' development. That is, we will spend much of the funds in incentivizing artists to join LiveCoins platform, with includes offering subsidies, offering premium services, etc. The key note is tobring awareness, education and understanding of the advantages of the LiveCoins platform to artists.</div>
+				<div class="cf-content-image cfci2"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
