e9eafef68d8c3073daeeb3e1281238b161534b04
Ge Jian Feng <gejianfeng2@126.com>
2018-02-08 02:12:19 +0800

    + howlivecoinwork done

---
+++ en/howlivecoinswork.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Template Name: howlivecoinswork.php
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="hlcw">
+	<div class="header">
+		<?php get_top_banner(); ?>
+
+		<div class="wrap">
+			<div class="title">How Livecoins Works</div>
+			<div class="title-content">
+				Marketing system: less spending, higher conversion rate <br/> Pricing System: rational, incentive-compatible and efficient <br/>Service providers system: competitive, value-added and transparent
+			</div>
+			<div class="flow-image"></div>
+
+			<div class="info-frame if1">
+				<div class="if-title">Artist</div>
+				<div class="if-content">
+					<ul>
+						<li>Directly reach potential fans</li>
+						<li>Raise funds from fans</li>
+						<li>Regain values eroded by intermediaries</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="info-frame if2">
+				<div class="if-title">Fans</div>
+				<div class="if-content">
+					<ul>
+						<li>Lower ticket prices</li>
+						<li>Work directly with artist on live event</li>
+						<li>Support artists through secured and low-cost funding</li>
+						<li>More diversified artists and new artists</li>
+					</ul>
+				</div>
+			</div>
+
+			<div class="info-frame if3">
+				<div class="if-title">Service Providers</div>
+				<div class="if-content">
+					<ul>
+						<li>More chances to work with artists</li>
+						<li>A fair market to compete on value</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
