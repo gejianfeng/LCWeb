e9c4a13adee97b689b31c4e6f4f10da67e76eb8f
Ge Jian Feng <gejianfeng2@126.com>
2018-02-09 16:18:06 +0800

    + add header link

--- en/functions.php
+++ en/functions.php
@@ -4,19 +4,90 @@ function get_top_banner()
 {
 	$ret = '';
 	$ret .= "<div id='banner'>";
-		$ret .= "<div class='logo'></div>";
+		$ret .= "<div class='logo' style='cursor:pointer;' onclick='SwitchPage(0)'></div>";
 		$ret .= "<div class='english navsize'>English</div>";
 		$ret .= "<div class='lang-split'>|</div>";
 		$ret .= "<div class='chinese navsize text-transparent'>Chinese</div>";
 
-		$ret .= "<div class='contacts navsize text-transparent'>CONTACTS</div>";
-		$ret .= "<div class='faq navsize text-transparent'>FAQ</div>";
-		$ret .= "<div class='team navsize text-transparent'>TEAM</div>";
-		$ret .= "<div class='documents navsize text-transparent'>DOCUMENTS</div>";
-		$ret .= "<div class='roadmap navsize text-transparent'>ROADMAP</div>";
-		$ret .= "<div class='tokensale navsize text-transparent'>TOKEN SALE</div>";
-		$ret .= "<div class='howworks navsize text-transparent'>HOW LIVECOINS WORKS</div>";
-		$ret .= "<div class='whattosolve navsize text-transparent'>WHAT TO SOLVE</div>";
+		if (is_page('contact')) 
+		{
+			$ret .= "<div class='contacts navsize'>CONTACTS</div>";
+		}
+		else
+		{
+			$ret .= "<div class='contacts navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(8)'>CONTACTS</div>";
+		}
+
+		if (is_page('document')) 
+		{
+			$ret .= "<div class='documents navsize'>DOCUMENTS</div>";
+		}
+		else
+		{
+			$ret .= "<div class='documents navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(5)'>DOCUMENTS</div>";
+		}
+
+		if (is_page('faq')) {
+			$ret .= "<div class='faq navsize'>FAQ</div>";
+		}
+		else
+		{
+			$ret .= "<div class='faq navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(7)'>FAQ</div>";
+		}
+
+		if (is_page('howlivecoinswork')) 
+		{
+			$ret .= "<div class='howworks navsize'>HOW LIVECOINS WORKS</div>";
+		}
+		else
+		{
+			$ret .= "<div class='howworks navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(2)'>HOW LIVECOINS WORKS</div>";
+		}
+
+		if (is_page('roadmap')) 
+		{
+			$ret .= "<div class='roadmap navsize'>ROADMAP</div>";
+		}
+		else
+		{
+			$ret .= "<div class='roadmap navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(4)'>ROADMAP</div>";
+		}
+
+		if (is_page('team')) 
+		{
+			$ret .= "<div class='team navsize'>TEAM</div>";
+		}
+		else
+		{
+			$ret .= "<div class='team navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(6)'>TEAM</div>";
+		}
+
+		if (is_page('tokeninfo')) 
+		{
+			$ret .= "<div class='tokensale navsize'>TOKEN SALE</div>";
+		}
+		else
+		{
+			$ret .= "<div class='tokensale navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(3)'>TOKEN SALE</div>";
+		}
+
+		if (is_page('whattosolve')) 
+		{
+			$ret .= "<div class='whattosolve navsize'>WHAT TO SOLVE</div>";
+		}
+		else
+		{
+			$ret .= "<div class='whattosolve navsize text-transparent' style='cursor:pointer;' onclick='SwitchPage(1)'>WHAT TO SOLVE</div>";
+		}
+
+		//$ret .= "<div class='contacts navsize text-transparent'>CONTACTS</div>";
+		//$ret .= "<div class='faq navsize text-transparent'>FAQ</div>";
+		//$ret .= "<div class='team navsize text-transparent'>TEAM</div>";
+		//$ret .= "<div class='documents navsize text-transparent'>DOCUMENTS</div>";
+		//$ret .= "<div class='roadmap navsize text-transparent'>ROADMAP</div>";
+		//$ret .= "<div class='tokensale navsize text-transparent'>TOKEN SALE</div>";
+		//$ret .= "<div class='howworks navsize text-transparent'>HOW LIVECOINS WORKS</div>";
+		//$ret .= "<div class='whattosolve navsize text-transparent'>WHAT TO SOLVE</div>";
 
 	$ret .= "</div>";
 	echo $ret;
--- en/header.php
+++ en/header.php
@@ -13,6 +13,45 @@ Template Name: header.php
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 		<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js" type="text/javascript"></script>
+
+		<script text="text/javascript">
+			function SwitchPage(index)
+			{
+				$target_link = '';
+				switch(index)
+				{
+					case 0:
+						$target_link = '/index.php';
+						break;
+					case 1:
+						$target_link = '/index.php/whattosolve/';
+						break;
+					case 2:
+						$target_link = '/index.php/howlivecoinswork/';
+						break;
+					case 3:
+						$target_link = '/index.php/tokeninfo/';
+						break;
+					case 4:
+						$target_link = '/index.php/roadmap/';
+						break;
+					case 5:
+						$target_link = '/index.php/document/';
+						break;
+					case 6:
+						$target_link = '/index.php/team/';
+						break;
+					case 7:
+						$target_link = '/index.php/faq/';
+						break;
+					case 8:
+						$target_link = '/index.php/contact/';
+						break;
+					}
+
+				window.location.href='<?php echo home_url(); ?>' + $target_link;
+			}
+		</script>
 	</head>
 
 	<body>
